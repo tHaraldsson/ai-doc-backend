3e65cd3bb831e5d44afa5b5eae35fa89cd20f84b
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-09-30 16:41:33 +0200

    ADD: project structure and local DB connection

---
+++ src/main/java/com/haraldsson/aidocbackend/AiDocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.haraldsson.aidocbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDocBackendApplication.class, args);
+    }
+
+}
